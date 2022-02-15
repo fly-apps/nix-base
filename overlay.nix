9af983c7ddb968d32d294ad04da0fcf844ce8f85
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-02-15 14:07:08 -0500

    overlay: Add ruby_2_7_5 and ruby_2_7_4 as examples
    This relies on the upstream builder, which does not provide exactly
    great ergonomics for overrids.
    
    We have to use the tagged build, rather than the released tarball. This
    is because the default build relies on it to correcly configure
    `useRailsExpress`. Turning this off would allow relying on the released
    tarballs.
    
    Building other minor releases (e.g. 2.6.*) will not work as overriding
    `2_7` brings in patches that are unlikely to apply.
    
    For this, relying on a local copy of the builders will be necessary.

--- overlay.nix
+++ overlay.nix
@@ -1,2 +1,21 @@
 final: super: {
+  ruby_2_7_4 = final.ruby_2_7.overrideAttrs(_: {
+    version = "2.7.4";
+    src = (final.fetchFromGitHub {
+      owner = "ruby";
+      repo = "ruby";
+      rev = "v2_7_4";
+      sha256 = "sha256-t7hcM9cjp8z1neijl4lARAJwJJikVHqVLJMoOjnOOt8=";
+    });
+  });
+
+  ruby_2_7_5 = final.ruby_2_7.overrideAttrs(_: {
+    version = "2.7.5";
+    src = (final.fetchFromGitHub {
+      owner = "ruby";
+      repo = "ruby";
+      rev = "v2_7_5";
+      sha256 = "sha256-cC3JV/wbmQg1U9ZFVNXb1JCvoa/kNINNhoGEQ70rppg=";
+    });
+  });
 }
