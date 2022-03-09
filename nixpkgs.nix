1db4bf1c9f78a04b4e27c71ce1b4857eb3689a60
Joshua Sierles <joshua@fly.io>
2022-03-09 10:43:36 +0100

    Track nixos-unstable instead of a release

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -2,14 +2,14 @@
 # This can be replaced by any method of tracking inputs.
 #   - niv     https://github.com/nmattia/niv
 #   - npins   https://github.com/andir/npins
-#   - Flakes  https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake.html 
+#   - Flakes  https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake.html
 #
 # Using this simple purely-Nix shim serves as a placeholder.
 
 let
-  # Tracking https://github.com/NixOS/nixpkgs/tree/nixos-21.11
-  rev = "c28fb0a4671ff2715c1922719797615945e5b6a0"; # nixos-21.11
-  sha256 = "sha256:1qzvhxcsxb6s410xlfs4ggcvm1xbbd4jrazy6cpxc1rkrxbyz0kk";
+  # Tracking https://github.com/NixOS/nixpkgs/tree/unstable
+  rev = "062a0c5437b68f950b081bbfc8a699d57a4ee026"; # nixos-unstable
+  sha256 = "sha256:6575f6d59578d34ebc7dd9a3959018fe116817effab49a6caadc2fea6d98bfd5";
   tarball = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
     inherit sha256;
