e1afa6b54275cefd6e2dc4c8236752cf4042bd3a
Joshua Sierles <joshua@fly.io>
2022-03-09 10:29:56 +0100

    remove bad ruby entries

--- overlay.nix
+++ overlay.nix
@@ -25,25 +25,4 @@ in
       });
     };
   });
-
-  ruby_2_7_3 = final.ruby_2_7.overrideAttrs(_: {
-    version = "2.7.3";
-    src = (final.fetchFromGitHub {
-      owner = "ruby";
-      repo = "ruby";
-      rev = "v2_7_3";
-      sha256 = "sha256-cC3JV/wbmQg1U9ZFVNXb1JCvoa/kNINNhoGEQ70rppg=";
-    });
-  });
-
-  ruby_2_7_2 = final.ruby_2_7.overrideAttrs(_: {
-    version = "2.7.2";
-    src = (final.fetchFromGitHub {
-      owner = "ruby";
-      repo = "ruby";
-      rev = "v2_7_2";
-      sha256 = "sha256-cC3JV/wbmQg1U9ZFVNXb1JCvoa/kNINNhoGEQ70rppg=";
-    });
-  });
-
 }
