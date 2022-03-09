852979c745f474b160464fe32fb5f61bcdc208ae
Joshua Sierles <joshua@fly.io>
2022-03-09 11:21:07 +0100

    track nixpkgs master

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -7,9 +7,9 @@
 # Using this simple purely-Nix shim serves as a placeholder.
 
 let
-  # Tracking https://github.com/NixOS/nixpkgs/tree/unstable
-  rev = "062a0c5437b68f950b081bbfc8a699d57a4ee026"; # nixos-unstable
-  sha256 = "sha256:0vfd7g1gwy9lcnnv8kclqr68pndd9sg0xq69h465zbbzb2vnijh9";
+  # Tracking https://github.com/NixOS/nixpkgs/tree/master
+  rev = "d44916d12f1d39baa02325040b381311364ad93a";
+  sha256 = "sha256:1rqvacv514j52wxq1cy7b86dmyacs3hgx4rw6agjdba73ajrjshw";
   tarball = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
     inherit sha256;
