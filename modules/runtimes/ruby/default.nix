d1f6b4a8ee572587cb4b52b9144d3a17e8a9cbf1
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-03-10 14:16:17 -0500

    modules/runtimes: Add ruby runtime config
    Currently adds the Nixpkgs-native rubies, and the two sample
    Fly-specific overlaid rubies as valid inputs.
    
    This implementation provides "typedly" useful errors:
    
    ```
    error: A definition for option `runtimes.ruby.version' is not of type
    `one of "2.7.4", "2.7.5", "3.0.3", "3.1.0"'.
    ```
    
    This was gotten when trying to use a Ruby version not declared as
    available.
    
    Work could be done to provide a custom specialization on the type where
    the error message is made more useful for runtime selection.
    
    Fixes https://github.com/fly-apps/nix-base/issues/12

---
+++ modules/runtimes/ruby/default.nix
@@ -0,0 +1,69 @@
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (lib)
+    mkDefault
+    mkForce
+    mkMerge
+    mkOption
+    types
+  ;
+  inherit (config.runtimes.ruby)
+    rubyInterpreters
+  ;
+in
+{
+  options = {
+    runtimes = {
+      ruby = {
+        version = mkOption {
+          type = types.enum (builtins.attrNames rubyInterpreters);
+          default = pkgs.ruby.version.majMinTiny;
+          description = ''
+            Version of the Ruby interpreter to use.
+          '';
+        };
+        package = mkOption {
+          type = types.package;
+          internal = true;
+          description = ''
+            The selected Ruby package.
+
+            Note that overriding this option with a Ruby package will not
+            intrinsically configure it with version or features customization.
+
+            When overriden, the overriden version will be used as-is.
+          '';
+        };
+        rubyInterpreters = mkOption {
+          type = with types; attrsOf package;
+          description = ''
+            Definition of known Ruby interpreters
+          '';
+          internal = true;
+        };
+      };
+    };
+  };
+
+  config = {
+    runtimes.ruby = {
+      package = mkDefault (rubyInterpreters."${config.runtimes.ruby.version}");
+      rubyInterpreters = mkMerge [
+        # Ruby packages maintained in the Fly overlay
+        {
+          "2.7.4" = pkgs.ruby_2_7_4;
+          "2.7.5" = pkgs.ruby_2_7_5;
+        }
+        # Force the default `ruby` packages from Nixpkgs in the attrset.
+        # It is important for the default `pkgs.ruby.version.majMinTiny` usage
+        # in the default value for `runtimes.ruby.version`.
+        {
+          "${pkgs.ruby_2_7.version.majMinTiny}" = mkForce pkgs.ruby_2_7;
+          "${pkgs.ruby_3_0.version.majMinTiny}" = mkForce pkgs.ruby_3_0;
+          "${pkgs.ruby_3_1.version.majMinTiny}" = mkForce pkgs.ruby_3_1;
+        }
+      ];
+    };
+  };
+}
