9a5fe7baf7fc9012d43c84ea8947f4f70b7d00fa
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-03-10 14:59:24 -0500

    overlay: Add Ruby 3.1.1

--- overlay.nix
+++ overlay.nix
@@ -6,6 +6,7 @@ in
 {
   ruby_2_7_4 = callPackage ./pkgs/ruby/2.7.4.nix { };
   ruby_2_7_5 = callPackage ./pkgs/ruby/2.7.5.nix { };
+  ruby_3_1_1 = callPackage ./pkgs/ruby/3.1.1.nix { };
 
   defaultGemConfig = callPackage ./pkgs/ruby/gem-config.nix {
     defaultGemConfig = super.defaultGemConfig;
