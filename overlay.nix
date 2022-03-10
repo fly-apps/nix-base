eeebc284d423d2ac0723779781d8e2fe7d6906af
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-03-10 15:17:15 -0500

    overlay: Work around `callPackage` masking `.override`

--- overlay.nix
+++ overlay.nix
@@ -2,11 +2,31 @@ final: super:
 
 let
   inherit (final) callPackage;
+  inherit (final.lib) genAttrs;
+
+  #
+  # Simple wrapper around `import` that works like `callPackage`, but does not
+  # shadow `override`.
+  # When using `callPackage, `callPackage` adds its own `override` function to
+  # override the inputs. This can be problematic if the overlaid expression is
+  # intended to be further manipulated as-is.
+  #
+  # Use this when overriding versions in packages from Nixpkgs.
+  # Do not use for newly added packages or helpers.
+  #
+  callPackageNotOverridable = file: args:
+    let 
+      fn = import file;
+      fnArgs = builtins.attrNames (builtins.functionArgs fn);
+      selectedArgs = genAttrs fnArgs (name: final."${name}");
+    in
+    fn selectedArgs
+  ;
 in
 {
-  ruby_2_7_4 = callPackage ./pkgs/ruby/2.7.4.nix { };
-  ruby_2_7_5 = callPackage ./pkgs/ruby/2.7.5.nix { };
-  ruby_3_1_1 = callPackage ./pkgs/ruby/3.1.1.nix { };
+  ruby_2_7_4 = callPackageNotOverridable ./pkgs/ruby/2.7.4.nix { };
+  ruby_2_7_5 = callPackageNotOverridable ./pkgs/ruby/2.7.5.nix { };
+  ruby_3_1_1 = callPackageNotOverridable ./pkgs/ruby/3.1.1.nix { };
 
   defaultGemConfig = callPackage ./pkgs/ruby/gem-config.nix {
     defaultGemConfig = super.defaultGemConfig;
