979c4fb439104a133ab62174c115ed9b6c74f47d
Joshua Sierles <joshua@fly.io>
2022-04-06 20:37:53 +0200

    use nix.toml instead of fly.toml

--- docker/nix-base.nix
+++ docker/nix-base.nix
@@ -7,7 +7,7 @@
 # Using this simple purely-Nix shim serves as a placeholder.
 
 let
-  toml = (builtins.fromTOML (builtins.readFile ../fly.toml));
+  toml = (builtins.fromTOML (builtins.readFile nix.toml));
   rev = toml.build.nix_base_revision or "7f455becd1491747d88c0bdb01deb81f5d2a3024";
   sha256 = toml.build.nix_base_sha256 or "sha256:1nw5l177byk2p69cdc9s5hps1qd3llgg0pppv4b9avlpw5x1wlz7";
   tarball = builtins.fetchTarball {
